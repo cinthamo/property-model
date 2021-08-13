4bff82ffc14b5c582abc07b17d3f9fcd4c6653a1
Cristian Inthamoussu <cinthamo@gmail.com>
2021-08-13 09:24:18 -0300

    Attempt 2

--- haskell/Model/Property.hs
+++ haskell/Model/Property.hs
@@ -1,12 +1,18 @@
 module Model.Property where
 
+import Model.Property
+import Model.Resolver
+
 type Name = String
 data Value obj = Data String |
                  Ref Name |
-                 Obj obj
+                 Obj obj |
+                 Res Resolver |
+                 Lis Listener
 
 class PropertiesObject obj where
     get :: Name -> obj -> Maybe (Value obj)
     has :: Name -> obj -> Bool
     set :: Name -> Value obj -> obj -> obj
     clear :: Name -> obj -> obj
+    empty :: obj
--- haskell/Model/PropertyMap.hs
+++ haskell/Model/PropertyMap.hs
@@ -5,30 +5,30 @@ import qualified Data.Map as Map
 import Model.Property
 import Model.Resolver
 
-data PropertiesMap behaviour = PM behaviour (Map Name (Value (PropertiesMap behaviour)))
+data PropertiesMap = PM (Map Name (Value (PropertiesMap)))
 
-type PropertyGet behaviour = Name -> PropertiesMap behaviour -> Maybe (Value (PropertiesMap behaviour))
+type PropertyGet = Name -> PropertiesMap -> Maybe (Value (PropertiesMap))
 
 --- Properties as Bag ---
 
-getAsBag:: PropertyGet behaviour
+getAsBag:: PropertyGet
 getAsBag name (PM b map) = Map.lookup name map
 
-setAsBag :: Name -> Value (PropertiesMap behaviour) -> PropertiesMap behaviour -> PropertiesMap behaviour
+setAsBag :: Name -> Value PropertiesMap -> PropertiesMap -> PropertiesMap
 setAsBag name value (PM b map) = PM b (Map.insert name value map)
 
-hasAsBag :: Name -> PropertiesMap behaviour -> Bool
+hasAsBag :: Name -> PropertiesMap -> Bool
 hasAsBag name (PM b map) = Map.member name map
 
-clearAsBag :: Name -> PropertiesMap behaviour -> PropertiesMap behaviour
+clearAsBag :: Name -> PropertiesMap -> PropertiesMap
 clearAsBag name (PM b map) = PM b (Map.delete name map)
 
 --- Properties with Resolvers ---
 
-getContext :: behaviour -> PropertiesMap instanceBehaviour -> PropertiesMap behaviour
+getContext :: behaviour -> PropertiesMap -> PropertiesMap
 getContext b o = set "instance" (Obj o) (emptyMap b)
 
-getWithResolver:: Resolver behaviour => PropertyGet behaviour -> PropertiesMap behaviour -> PropertyGet behaviour
+getWithResolver:: Resolver behaviour => PropertyGet -> PropertiesMap -> PropertyGet
 getWithResolver valueGet context = \name obj@(PM resolver map) ->
         case (beforeGet context name resolver) of
             GNotResolved -> Nothing
@@ -47,11 +47,9 @@ getWithResolver valueGet context = \name obj@(PM resolver map) ->
 
 --- PropertiesMap Instance ---
 
-emptyMap :: behaviour -> PropertiesMap behaviour
-emptyMap b = PM b Map.empty
-
-instance PropertiesObject (PropertiesMap behaviour) where
+instance PropertiesObject PropertiesMap where
     get name obj = getWithResolver getAsBag (getContext "" obj) name obj
     set = setAsBag
     has = hasAsBag
     clear = clearAsBag
+    emptyMap = PM Map.empty
