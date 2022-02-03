8bdbcf077071c2f33947022aa4f4c92295da48a7
Cristian Inthamoussu <cinthamo@gmail.com>
2022-02-03 12:14:59 -0300

    Generate Default Resolver

--- haskell2/src/Generator/Convert.hs
+++ haskell2/src/Generator/Convert.hs
@@ -1,5 +1,6 @@
 module Generator.Convert (convert) where
 
+import Data.List
 import Model.Definition
 import Model.Value
 import Generator.Data as D
@@ -20,7 +21,7 @@ convertOne :: Definition -> GDefinition
 convertOne (Definition name _type _default apply readonly valid) = GDefinition {
         D.id = name,
         D.name = name,
-        D.aType = convertType _type,
+        D.aType = convertTypeGxp _type,
         D.customType = convertCustomType _type,
         D.aDefault = convertDefault _default,
         D.defaultResolver = convertDefaultResolver _default,
@@ -29,12 +30,12 @@ convertOne (Definition name _type _default apply readonly valid) = GDefinition {
         D.validResolver = convertValidResolver valid
     }
 
-convertType :: ValueType -> String
-convertType TString = "Text"
-convertType TNumber = "Integer"
-convertType TBool = "Boolean"
-convertType TUnknown = error "Can't generate a property with unknown type"
-convertType (TCustom s) = "Custom"
+convertTypeGxp :: ValueType -> String
+convertTypeGxp TString = "Text"
+convertTypeGxp TNumber = "Integer"
+convertTypeGxp TBool = "Boolean"
+convertTypeGxp TUnknown = error "Can't generate a property with unknown type"
+convertTypeGxp (TCustom s) = "Custom"
 
 convertCustomType :: ValueType -> Maybe String
 convertCustomType (TCustom s) = Just s
@@ -66,7 +67,88 @@ convertDefaultResolvers props = map convertDefaultResolversOne $ filter needReso
         needResolver _ = False
 
 convertDefaultResolversOne :: Definition -> GDefaultResolver
-convertDefaultResolversOne (Definition name _ _ _ _ _) = GDefaultResolver {
+convertDefaultResolversOne (Definition name _ _default _ _ _) = GDefaultResolver {
         D.propName = name,
-        D.className = name ++ "DefaultResolver"
+        D.className = name ++ "DefaultResolver",
+        D.code = convertCode _default,
+        D.used = nub $ usedProperties _default
     }
+
+convertCode :: Expr -> [GStatement]
+convertCode (Case [(condition,result)] otherwise) = [
+        GStatement {
+            assign = Nothing,
+            aIf = Just (GIf {
+                D.condition = convertExpr condition,
+                D.trueBlock = convertCode result,
+                D.falseBlock = fmap convertCode otherwise
+            })
+        }
+    ]
+convertCode (Case ((condition,result):more) otherwise) = [
+        GStatement {
+            assign = Nothing,
+            aIf = Just (GIf {
+                D.condition = convertExpr condition,
+                D.trueBlock = convertCode result,
+                D.falseBlock = Just (convertCode (Case more otherwise))
+            })
+        }
+    ]
+convertCode e = [
+        GStatement {
+            assign = Just (GAssign {
+                D.varName = "value",
+                D.expr = convertExpr e
+            }),
+            aIf = Nothing
+        }
+    ]
+
+usedProperties :: Expr -> [String]
+usedProperties (Ref _ prop) = [prop]
+usedProperties (Case conditions otherwise) = (concat $ map (usedProperties . fst) conditions) ++ (concat $ map (usedProperties . snd) conditions) ++ f otherwise
+    where
+        f Nothing = []
+        f (Just x) = usedProperties x
+usedProperties (Call _ param) = concat $ map usedProperties param
+usedProperties _ = []
+
+convertExpr :: Expr -> GExpr
+convertExpr (Value (V v)) = GExpr {
+        constant = Just $ convertValue v,
+        getProp = Nothing,
+        call = Nothing,
+        operator = Nothing
+    }
+convertExpr (Ref "this" prop) = GExpr {
+        constant = Nothing,
+        getProp = Just $ GGetProp (convertTypeDotNet prop) prop,
+        call = Nothing,
+        operator = Nothing
+    }
+convertExpr (Call name lexpr) =
+    case (elem name operators) of
+        False -> GExpr {
+            constant = Nothing,
+            getProp = Nothing,
+            call = Just $ GCall name (map convertExpr lexpr),
+            operator = Nothing
+        }
+        True -> GExpr {
+            constant = Nothing,
+            getProp = Nothing,
+            call = Nothing,
+            operator = Just $ GOperator name (convertExpr $ head lexpr) (convertExpr $ head $ tail lexpr)
+        }
+convertExpr e = error $ "Not yet supported expression " ++ (show e)
+
+convertValue :: Value obj -> String
+convertValue (String s) = "\"" ++ s ++ "\""
+convertValue (Number n) = show n
+
+convertTypeDotNet :: String -> String
+convertTypeDotNet _ = "int"
+
+operators :: [String]
+operators = ["==", "+"]
--- haskell2/src/Generator/Data.hs
+++ haskell2/src/Generator/Data.hs
@@ -28,6 +28,64 @@ data GDefaultResolver
   = GDefaultResolver
       {
         propName :: String,
-        className :: String
+        className :: String,
+        code :: [GStatement],
+        used :: [String]
       }
+  deriving (Show, Data, Typeable)
+
+data GStatement
+  = GStatement
+      {
+        assign :: Maybe GAssign,
+        aIf :: Maybe GIf
+      }
+  deriving (Show, Data, Typeable)
+
+data GAssign
+  = GAssign
+      {
+        varName :: String,
+        expr :: GExpr
+      }
+  deriving (Show, Data, Typeable)
+  
+data GIf
+  = GIf
+      {
+        condition :: GExpr,
+        trueBlock :: [GStatement],
+        falseBlock :: Maybe [GStatement]
+      }
+  deriving (Show, Data, Typeable)
+
+data GExpr
+  = GExpr {
+    constant :: Maybe String,
+    getProp :: Maybe GGetProp,
+    call :: Maybe GCall,
+    operator :: Maybe GOperator
+  }
+  deriving (Show, Data, Typeable)
+
+data GGetProp
+  = GGetProp {
+    pType :: String,
+    pName :: String
+  }
+  deriving (Show, Data, Typeable)
+
+data GCall
+  = GCall {
+    fname :: String,
+    param :: [GExpr]
+  }
+  deriving (Show, Data, Typeable)
+
+data GOperator
+  = GOperator {
+    symbol :: String,
+    left :: GExpr,
+    right :: GExpr
+  }
   deriving (Show, Data, Typeable)
